73e754383f6150babb0305ae11c514159a804df5
AFPedreros <felipe.pedreros94@gmail.com>
2022-11-04 10:25:00 -0500

    making a basic NFT contract

---
+++ deploy/01-deploy-basic-nft.js
@@ -0,0 +1,5 @@
+const { network } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async function ({ getNamedAccounts, deployments }) {}
