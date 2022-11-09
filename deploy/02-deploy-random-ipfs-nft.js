941d351a6bdef843cebcfa73748506cd6dc47527
AFPedreros <felipe.pedreros94@gmail.com>
2022-11-09 16:48:01 -0500

    upload NFT images to pinata

--- deploy/02-deploy-random-ipfs-nft.js
+++ deploy/02-deploy-random-ipfs-nft.js
@@ -1,11 +1,33 @@
 const { network, ethers } = require("hardhat")
 const { developmentChains } = require("../helper-hardhat-config")
 const { verify } = require("../utils/verify")
+const { storeImages } = require("../utils/uploadToPinata")
+
+const imagesLocation = "./images/randomNFT"
+
+const metadataTemplate = {
+    name: "",
+    description: "",
+    image: "",
+    attributes: [
+        {
+            trait_types: "Cuteness",
+            value: 100,
+        },
+    ],
+}
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
+    let tokenUris
+
+    // Get the IPFS hashes of our images
+
+    if (process.env.UPLOAD_TO_PINATA == true) {
+        tokenUris = await handleTokenUris()
+    }
 
     let vrfCoordinatorV2Address, subscriptionId
 
@@ -15,7 +37,7 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
         )
         vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
         const tx = await vrfCoordinatorV2Mock.createSubscription()
-        const transactionReceipt = await transactionResponse.wait(1)
+        const transactionReceipt = await tx.wait(1)
         subscriptionId = transactionReceipt.events[0].args.subId
     } else {
         vrfCoordinatorV2Address = networkConfig[chainId].vrfCoordinatorV2
@@ -23,4 +45,21 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
     }
 
     log("----------------------------------------------------------")
+    await storeImages(imagesLocation)
+    // const args = [
+    //     vrfCoordinatorV2Address,
+    //     subscriptionId,
+    //     networkConfig[chainId].gasLane,
+    //     networkConfig[chainId].callbackGasLimit,
+    //     // tokenUris
+    //     networkConfig[chainId].mintFee,
+    // ]
 }
+
+async function handleTokenUris() {
+    let tokenUris = []
+
+    return tokenUris
+}
+
+module.exports.tags = ["all", "randomipfs", "main"]
---
+++ utils/uploadToPinata.js
@@ -0,0 +1,36 @@
+require("dotenv").config()
+const pinataSDK = require("@pinata/sdk")
+const fs = require("fs")
+const path = require("path")
+
+const pinataApiKey = process.env.PINATA_API_KEY
+const pinataApiSecret = process.env.PINATA_API_SECRET
+const pinata = new pinataSDK(pinataApiKey, pinataApiSecret)
+
+async function storeImages(imagesFilePath) {
+    const fullImagesPath = path.resolve(imagesFilePath)
+    const files = fs.readdirSync(fullImagesPath)
+    let responses = []
+    console.log("Uploading to Pinata!")
+    for (fileIndex in files) {
+        const readableStreamForFile = fs.createReadStream(
+            `${fullImagesPath}/${files[fileIndex]}`
+        )
+        try {
+            const response = await pinata.pinFileToIPFS(readableStreamForFile, {
+                pinataMetadata: {
+                    name: `${files[fileIndex]}`,
+                },
+            })
+            console.log(response)
+            responses.push(response)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    return { responses, files }
+}
+
+async function storeTokenUriMetadata(metadata) {}
+
+module.exports = { storeImages }
